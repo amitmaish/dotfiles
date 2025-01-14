ad8e5281636cbabb6bf9d5d96f383583f12d8947
amit maish <amit.maish1@gmail.com>
2025-01-14 08:14:06 -0800

    added in nvim

---
+++ .config/nvim/lua/vim-options.lua
@@ -0,0 +1,9 @@
+vim.cmd("set noexpandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.wo.number = true
+
+vim.g.mapleader = ","
+
+vim.keymap.set("i", "jk", "<Esc>")
