55b2f1f0906b21051a4ec00ffb02e907d19d642e
AmitMaish <83043520+AmitMaish@users.noreply.github.com>
2025-01-15 18:10:53 -0800

    added nvim-tmux integration

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -10,3 +10,9 @@ vim.keymap.set("i", "jk", "<Esc>")
 
 vim.keymap.set("n", "<S-h>", "0")
 vim.keymap.set("n", "<S-l>", "$")
+
+-- Navigate vim panes better
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", { silent = true })
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", { silent = true })
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", { silent = true })
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", { silent = true })
