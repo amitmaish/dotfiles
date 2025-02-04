6780a99bcc04c86a2f04fcf90b619b381970d722
AmitMaish <83043520+AmitMaish@users.noreply.github.com>
2025-02-04 14:47:36 -0800

    improved the hotkeys for vim commentary

--- .config/nvim/lua/plugins/commentary.lua
+++ .config/nvim/lua/plugins/commentary.lua
@@ -3,7 +3,7 @@ return {
 		"tpope/vim-commentary",
 		config = function()
 			vim.keymap.set("n", "÷", "gcc", { remap = true })
-			vim.keymap.set("i", "÷", "<esc>gcc`^i", { remap = true })
+			vim.keymap.set("i", "÷", "<esc>gcc`^llli", { remap = true })
 			vim.keymap.set("v", "÷", "gc", { remap = true })
 		end,
 	},
