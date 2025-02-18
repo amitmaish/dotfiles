f76b15201d7df7f73ef60d068f6cab457c855212
AmitMaish <83043520+AmitMaish@users.noreply.github.com>
2025-02-18 14:40:11 -0800

    added a hotkey to inject metadata in neorg

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -20,5 +20,6 @@ return {
 		})
 		vim.keymap.set("n", "<localleader>ni", ":Neorg index<cr>")
 		vim.keymap.set("n", "<localleader>nr", ":Neorg return<cr>")
+		vim.keymap.set("n", "<localleader>nm", ":Neorg inject-metadata<cr>")
 	end,
 }
