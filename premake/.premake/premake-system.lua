17ed2eed7977fcb12e1b689b713174be1d844105
AmitMaish <83043520+AmitMaish@users.noreply.github.com>
2025-04-15 14:59:44 -0700

    configuring premake

---
+++ premake/.premake/premake-system.lua
@@ -0,0 +1,5 @@
+require("export-compile-commands")
+require("cmake")
+
+-- clone the folowing repository
+-- git clone https://github.com/tarruda/premake-export-compile-commands export-compile-commands
