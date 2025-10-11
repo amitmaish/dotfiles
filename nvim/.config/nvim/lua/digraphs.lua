a25685f2d6fcd68c0d287457af041eafb38b5a5e
amitmaish <83043520+AmitMaish@users.noreply.github.com>
2025-10-11 13:34:10 -0700

    added more digraphs

--- nvim/.config/nvim/lua/digraphs.lua
+++ nvim/.config/nvim/lua/digraphs.lua
@@ -2,7 +2,11 @@ local digraph = vim.fn.digraph_set
 
 digraph("uh", "ʌ") -- stressed 'uh'
 digraph("uu", "ə") -- schwa
+digraph("ah", "ɔ") -- open o
+digraph("aa", "ɑ") -- script a
+digraph("ih", "ɪ") -- small capital I
 digraph("ns", "̯") -- non-sylabic diacritic
 digraph("na", "̃") -- nasal diacritic
 digraph("um", "̈") -- umlaut
 digraph("ww", "ʍ") -- "upside down capital W"
+digraph("eh", "ε") -- epsilon
