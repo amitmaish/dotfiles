2e870eebad271e2af60298cf020b70517625e5f2
amitmaish <83043520+AmitMaish@users.noreply.github.com>
2025-10-15 08:28:29 -0700

    reset term variable after yazi is run
    also changed to tabs from spaces

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,17 +2,18 @@ set MANPAGER "sh -c 'col -bx | bat -l man -p'"
 set EDITOR nvim
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+	# Commands to run in interactive sessions can go here
 end
 
 function y
-    set tmp (mktemp -t "yazi-cwd.XXXXXX")
-    set TERM xterm-kitty yazi
-    yazi $argv --cwd-file="$tmp"
-    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-        builtin cd -- "$cwd"
-    end
-    rm -f -- "$tmp"
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	set TERM xterm-kitty yazi
+	yazi $argv --cwd-file="$tmp"
+	if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+	set TERM xterm-kitty
 end
 
 alias ls="exa"
