123b837629b50e4e2a88e847989d166f22c93add
amitmaish <83043520+AmitMaish@users.noreply.github.com>
2025-10-15 13:23:06 -0700

    remove yazi TERM override

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,13 +7,11 @@ end
 
 function y
 	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	set TERM xterm-kitty yazi
 	yazi $argv --cwd-file="$tmp"
 	if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
 		builtin cd -- "$cwd"
 	end
 	rm -f -- "$tmp"
-	set TERM xterm-kitty
 end
 
 alias ls="exa"
