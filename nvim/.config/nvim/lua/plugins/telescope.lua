d3c0336e94bc029a35e2d872856178cc3a4d56b9
amitmaish <83043520+AmitMaish@users.noreply.github.com>
2025-10-17 10:26:28 -0700

    made telescope global

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,43 +2,43 @@ return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = { "nvim-lua/plenary.nvim", "davidgranstrom/telescope-scdoc.nvim" },
 	config = function(opts)
-		local telescope = require("telescope")
-		telescope.setup(opts)
-		telescope.load_extension("scdoc")
+		Telescope = require("telescope")
+		Telescope.setup(opts)
+		Telescope.load_extension("scdoc")
 	end,
 	keys = {
 		{
 			"<leader> ",
 			function()
-				vim.cmd.Telescope("find_files")
+				Telescope.builtin.find_files()
 			end,
 			desc = "telescope files",
 		},
 		{
 			"<leader>pf",
 			function()
-				vim.cmd.Telescope("git_files")
+				Telescope.builtin.git_files()
 			end,
 			desc = "telescope git files",
 		},
 		{
 			"<leader>ph",
 			function()
-				vim.cmd.Telescope("help_tags")
+				Telescope.builtin.help_tags()
 			end,
 			desc = "telescope help tags",
 		},
 		{
 			"<leader>pg",
 			function()
-				vim.cmd.Telescope("live_grep")
+				Telescope.builtin.live_grep()
 			end,
 			desc = "telescope grep",
 		},
 		{
 			"<leader>pb",
 			function()
-				vim.cmd.Telescope("builtin")
+				Telescope.builtin.builtin()
 			end,
 			desc = "telescope builtins",
 		},
