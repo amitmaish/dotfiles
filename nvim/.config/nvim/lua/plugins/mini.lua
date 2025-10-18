d548d78e3913344aa5df927cb008f1f9c99f370c
amitmaish <83043520+AmitMaish@users.noreply.github.com>
2025-10-18 11:28:13 -0700

    changed statusline function

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -7,7 +7,34 @@ return {
 		require("mini.comment").setup()
 		require("mini.icons").setup()
 		require("mini.pairs").setup()
-		require("mini.statusline").setup()
+		require("mini.statusline").setup({
+			content = {
+				active = function()
+					local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+					local git = MiniStatusline.section_git({ trunc_width = 40 })
+					local diff = MiniStatusline.section_diff({ trunc_width = 75 })
+					local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+					local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
+					local filename = MiniStatusline.section_filename({ trunc_width = 10 })
+					local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+					local location = MiniStatusline.section_location({ trunc_width = 75 })
+					local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
+
+					return MiniStatusline.combine_groups({
+						{ hl = mode_hl, strings = { string.lower(mode) } },
+						{ hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
+						"%<", -- Mark general truncate point
+						{
+							hl = "MiniStatuslineFilename",
+							strings = { vim.bo.filetype ~= "" and filename },
+						},
+						"%=", -- End left alignment
+						{ hl = "MiniStatuslineFileinfo", strings = { fileinfo:gsub("%[.*%]", "") } },
+						{ hl = mode_hl, strings = { search, location } },
+					})
+				end,
+			},
+		})
 		require("mini.surround").setup({
 			mappings = {
 				add = "gsa", -- Add surrounding in Normal and Visual modes
