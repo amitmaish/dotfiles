2d47827b0f59949a0f3f2df8b8fc94d32bca5deb
amitmaish <83043520+AmitMaish@users.noreply.github.com>
2025-10-20 10:41:37 -0700

    added a session picker to telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,21 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	dependencies = { "nvim-lua/plenary.nvim", "davidgranstrom/telescope-scdoc.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"JoseConseco/telescope_sessions_picker.nvim",
+		"davidgranstrom/telescope-scdoc.nvim",
+	},
+	opts = {
+		extensions = {
+			sessions_picker = {
+				sessions_dir = vim.fn.stdpath("data") .. "/session/",
+			},
+		},
+	},
 	config = function(opts)
 		local telescope = require("telescope")
 		telescope.setup(opts)
+		telescope.load_extension("sessions_picker")
 		telescope.load_extension("scdoc")
 	end,
 	keys = {
@@ -31,6 +43,13 @@ return {
 			"<leader>pb",
 			require("telescope.builtin").builtin,
 			desc = "telescope builtins",
+		},
+		{
+			"<leader>ps",
+			function()
+				vim.cmd.Telescope("sessions_picker")
+			end,
+			desc = "telescope sessions",
 		},
 	},
 }
