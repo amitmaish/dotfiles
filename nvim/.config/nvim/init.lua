c600109f68c09ee9eafdfcc92fc58d8952e75deb
amitmaish <83043520+AmitMaish@users.noreply.github.com>
2025-10-20 12:51:08 -0700

    fixed lsp_configs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,5 @@
 require("config.lazy")
+require("config.lsp_configs")
 require("digraphs")
 require("neovide")
 
---
+++ nvim/.config/nvim/lua/config/lsp_configs.lua
@@ -0,0 +1,41 @@
+vim.lsp.enable({
+	"asm_lsp",
+	"ast_grep",
+	"arduino_language_server",
+	"biome",
+	"clangd",
+	"gopls",
+	"hls",
+	"html",
+	"jsonls",
+	"lua_ls",
+	"marksman",
+	"nil_ls",
+	"ruff",
+	"taplo",
+	"texlab",
+	"tinymist",
+	"vtsls",
+	"yamlls",
+	"zls",
+})
+vim.lsp.config("arduino_language_server", {
+	cmd = {
+		"arduino-language-server",
+		"-clangd",
+		vim.fn.stdpath("data") .. "/mason/bin/clangd",
+		"-cli",
+		"/usr/local/bin/arduino-cli",
+		"-cli-config",
+		vim.fn.expand("$HOME/") .. ".arduinoIDE/arduino-cli.yaml",
+	},
+})
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+		},
+	},
+})
