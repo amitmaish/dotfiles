b1b768f33e55191ca6ce7d4c3017da10ef144952
amitmaish <83043520+AmitMaish@users.noreply.github.com>
2025-10-21 16:01:12 -0700

    added nvim runtime to .luarc.json

--- nvim/.config/nvim/lua/config/lsp_configs.lua
+++ nvim/.config/nvim/lua/config/lsp_configs.lua
@@ -30,14 +30,3 @@ vim.lsp.config("arduino_language_server", {
 		vim.env.HOME .. ".arduinoIDE/arduino-cli.yaml",
 	},
 })
-if vim.env.NVIM_LINK then
-	vim.lsp.config("lua_ls", {
-		settings = {
-			Lua = {
-				workspace = {
-					library = vim.api.nvim_get_runtime_file("", true),
-				},
-			},
-		},
-	})
-end
