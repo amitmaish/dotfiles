b4f73071a6afc9ee4e3d28c5d0e72e56fd30782f
amitmaish <83043520+AmitMaish@users.noreply.github.com>
2025-10-21 16:31:54 -0700

    changed how colorschemes are laid out

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -4,40 +4,38 @@ return {
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				auto_integrations = true,
-				custom_highlights = function(colors)
-					return {
-						["@property"] = { fg = colors.lavender },
-						["@markup.italic"] = { fg = colors.green },
-						["@markup.strong"] = { fg = colors.mauve },
-						["StatusLine"] = { bg = colors.none },
-						["StatusLineNC"] = { bg = colors.none },
+		opts = {
+			auto_integrations = true,
+			custom_highlights = function(colors)
+				return {
+					["@property"] = { fg = colors.lavender },
+					["@markup.italic"] = { fg = colors.green },
+					["@markup.strong"] = { fg = colors.mauve },
+					["StatusLine"] = { bg = colors.none },
+					["StatusLineNC"] = { bg = colors.none },
 
-						-- mini
-						["MiniStatuslineModeNormal"] = { fg = colors.blue, bg = colors.none },
-						["MiniStatuslineModeInsert"] = { fg = colors.green, bg = colors.none },
-						["MiniStatuslineModeVisual"] = { fg = colors.mauve, bg = colors.none },
-						["MiniStatuslineModeCommand"] = { fg = colors.peach, bg = colors.none },
-						["MiniStatuslineModeReplace"] = { fg = colors.red, bg = colors.none },
-						["MiniStatuslineModeOther"] = { fg = colors.teal, bg = colors.none },
-						["MiniStatuslineFilename"] = { link = "Comment" },
-						["MiniStatuslineFileinfo"] = { link = "Comment" },
-						["MiniStatuslineDevinfo"] = { link = "Comment" },
-					}
-				end,
-			})
-			vim.cmd.colorscheme("catppuccin")
-		end,
+					-- mini
+					["MiniStatuslineModeNormal"] = { fg = colors.blue, bg = colors.none },
+					["MiniStatuslineModeInsert"] = { fg = colors.green, bg = colors.none },
+					["MiniStatuslineModeVisual"] = { fg = colors.mauve, bg = colors.none },
+					["MiniStatuslineModeCommand"] = { fg = colors.peach, bg = colors.none },
+					["MiniStatuslineModeReplace"] = { fg = colors.red, bg = colors.none },
+					["MiniStatuslineModeOther"] = { fg = colors.teal, bg = colors.none },
+					["MiniStatuslineFilename"] = { link = "Comment" },
+					["MiniStatuslineFileinfo"] = { link = "Comment" },
+					["MiniStatuslineDevinfo"] = { link = "Comment" },
+				}
+			end,
+			init = function()
+				vim.cmd.colorscheme("catppuccin")
+			end,
+		},
 	},
 	---@type LazyPluginSpec
 	{
 		name = "sakura-crush",
 		dev = true,
 		dir = "~/dev/sakura-crush",
-		config = function()
-			require("sakura-crush").setup()
-		end,
+		opts = {},
 	},
 }
