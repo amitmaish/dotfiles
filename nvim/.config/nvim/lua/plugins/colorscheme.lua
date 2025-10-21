f8321fff381a900e3478ead89cc591d02ada6fde
amitmaish <83043520+AmitMaish@users.noreply.github.com>
2025-10-20 18:23:53 -0700

    updated catppuccin

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,31 +2,29 @@ return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
-	---@type CatppuccinOptions
-	opts = {
-		auto_integrations = true,
-		custom_highlights = function(colors)
-			return {
-				["@property"] = { fg = colors.lavender },
-				["@markup.italic"] = { fg = colors.green },
-				["@markup.strong"] = { fg = colors.mauve },
-				["statusline"] = { bg = colors.none },
+	config = function()
+		require("catppuccin").setup({
+			auto_integrations = true,
+			custom_highlights = function(colors)
+				return {
+					["@property"] = { fg = colors.lavender },
+					["@markup.italic"] = { fg = colors.green },
+					["@markup.strong"] = { fg = colors.mauve },
+					["statusline"] = { bg = colors.none },
 
-				-- mini
-				["MiniStatuslineModeNormal"] = { fg = colors.blue, bg = colors.none },
-				["MiniStatuslineModeInsert"] = { fg = colors.green, bg = colors.none },
-				["MiniStatuslineModeVisual"] = { fg = colors.mauve, bg = colors.none },
-				["MiniStatuslineModeCommand"] = { fg = colors.peach, bg = colors.none },
-				["MiniStatuslineModeReplace"] = { fg = colors.red, bg = colors.none },
-				["MiniStatuslineModeOther"] = { fg = colors.teal, bg = colors.none },
-				["MiniStatuslineFilename"] = { link = "Comment" },
-				["MiniStatuslineFileinfo"] = { link = "Comment" },
-				["MiniStatuslineDevinfo"] = { link = "Comment" },
-			}
-		end,
-	},
-	config = function(opts)
-		require("catppuccin").setup(opts)
+					-- mini
+					["MiniStatuslineModeNormal"] = { fg = colors.blue, bg = colors.none },
+					["MiniStatuslineModeInsert"] = { fg = colors.green, bg = colors.none },
+					["MiniStatuslineModeVisual"] = { fg = colors.mauve, bg = colors.none },
+					["MiniStatuslineModeCommand"] = { fg = colors.peach, bg = colors.none },
+					["MiniStatuslineModeReplace"] = { fg = colors.red, bg = colors.none },
+					["MiniStatuslineModeOther"] = { fg = colors.teal, bg = colors.none },
+					["MiniStatuslineFilename"] = { link = "Comment" },
+					["MiniStatuslineFileinfo"] = { link = "Comment" },
+					["MiniStatuslineDevinfo"] = { link = "Comment" },
+				}
+			end,
+		})
 		vim.cmd.colorscheme("catppuccin")
 	end,
 }
