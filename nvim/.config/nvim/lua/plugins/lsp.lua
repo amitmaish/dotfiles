0c3a45d9f8b4209b5187c25c2b4a5d3c4be1403d
amitmaish <83043520+AmitMaish@users.noreply.github.com>
2025-11-14 17:34:14 -0800

    moved rustaceanvim to its own file as well as adding clangd-extensions

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -16,21 +16,4 @@ return {
 		"https://github.com/rcarriga/nvim-dap-ui",
 		dependencies = { "https://github.com/mfussenegger/nvim-dap" },
 	},
-	{
-		"mrcjkb/rustaceanvim",
-		lazy = false,
-	},
-	{
-		"saecki/crates.nvim",
-		tag = "stable",
-		---@type crates.UserConfig
-		opts = {
-			lsp = {
-				enabled = true,
-				actions = true,
-				completion = true,
-				hover = true,
-			},
-		},
-	},
 }
