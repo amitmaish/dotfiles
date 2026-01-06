23a311f4b77c2cf27065f2a10974f02762455066
amit <amit.maish1@gmail.com>
2026-01-05 20:52:23 -0800

    nix - added winapps

--- nix/flake.nix
+++ nix/flake.nix
@@ -14,6 +14,10 @@
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    winapps = {
+      url = "github:winapps-org/winapps";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
@@ -21,10 +25,11 @@
     nixpkgs,
     home-manager,
     rust-overlay,
+    winapps,
     ...
   } @ inputs: {
-    nixosConfigurations.amit = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
+    nixosConfigurations.amit = nixpkgs.lib.nixosSystem rec {
+      specialArgs = {inherit inputs system;};
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
@@ -42,6 +47,18 @@
           nixpkgs.overlays = [rust-overlay.overlays.default];
           environment.systemPackages = [pkgs.rust-bin.stable.latest.default];
         })
+        (
+          {
+            pkgs,
+            system ? pkgs.system,
+            ...
+          }: {
+            environment.systemPackages = [
+              winapps.packages."${system}".winapps
+              winapps.packages."${system}".winapps-launcher # optional
+            ];
+          }
+        )
         ./modules/noctalia.nix
       ];
     };
