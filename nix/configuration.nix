3c9380995d00894ff1cc261ba01b6fcca6c51039
amit <amit.maish1@gmail.com>
2026-01-09 21:26:39 -0800

    nix - gen 221

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -227,6 +227,8 @@
   };
 
   services.tailscale.enable = true;
+  networking.nftables.enable = true;
+  networking.firewall.checkReversePath = "loose";
 
   networking.firewall.enable = true;
   networking.firewall.allowPing = true;
