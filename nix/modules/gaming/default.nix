7872c87323da8b0abe0a833a9757ecb7ea4fb5db
amit <amit.maish1@gmail.com>
2026-01-10 17:53:27 -0800

    nix - gen 236

--- nix/modules/gaming/default.nix
+++ nix/modules/gaming/default.nix
@@ -1,11 +1,18 @@
 {
-  config,
+  lib,
   pkgs,
   ...
 }: {
   imports = [
     ./emulation.nix
   ];
+
+  emulation.enable = lib.mkDefault true;
+  cemu = {
+    enable = lib.mkDefault true;
+    ukmm = lib.mkDefault true;
+  };
+  retroarch.enable = lib.mkDefault true;
 
   home.packages = with pkgs; [
     lutris
