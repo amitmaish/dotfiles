eeef8fb0cda8829c8f3c583f3c815885d9255ad2
amitmaish <83043520+AmitMaish@users.noreply.github.com>
2026-01-17 12:49:57 -0800

    nix - amit-mbp gen darwin-rebuild --list-generations | grep "current" | awk '{print $1}'

--- nix/hosts/amitmbp/home.nix
+++ nix/hosts/amitmbp/home.nix
@@ -90,7 +90,7 @@ in {
   };
 
   home.file.".config/bat".source = config.lib.file.mkOutOfStoreSymlink ../../../bat/.config/bat;
-  home.file.".config/ghostty".source = config.lib.file.mkOutOfStoreSymlink ../../../ghostty/.config/ghostty;
+  home.file.".config/ghostty".source = config.lib.file.mkOutOfStoreSymlink "~/dotfiles/ghostty/.config/ghostty";
   home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink ../../../nvim/.config/nvim;
   home.file.".config/starship.toml".source = config.lib.file.mkOutOfStoreSymlink ../../../starship/.config/starship.toml;
   home.file.".config/yazi".source = ../../../yazi/.config/yazi;
