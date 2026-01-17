41e1b5463c68706f4317f5751f3d0b994745d88c
amitmaish <83043520+AmitMaish@users.noreply.github.com>
2026-01-17 13:17:50 -0800

    nix - amit-mbp gen darwin-rebuild --list-generations | grep "current" | awk '{print $1}'

--- nix/hosts/amitmbp/system.nix
+++ nix/hosts/amitmbp/system.nix
@@ -1,23 +1,28 @@
 {pkgs, ...}: {
   system.defaults = {
-    dock.autohide = true;
-    dock.persistent-apps = [
-      "/System/Applications/Messages.app"
-      "/Applications/Signal.app"
-      "/Applications/Ghostty.app"
-      "/Applications/Firefox.app"
-      "/Applications/Anki.app"
-      "${pkgs.obsidian}/Applications/Obsidian.app"
-      "/System/Applications/Calendar.app"
-      "/Applications/Pianoteq 8"
-      "/Applications/Dorico 6.app"
-    ];
+    dock = {
+      autohide = true;
+      persistent-apps = [
+        "/System/Applications/Messages.app"
+        "/Applications/Signal.app"
+        "/Applications/Ghostty.app"
+        "/Applications/Firefox.app"
+        "/Applications/Anki.app"
+        "${pkgs.obsidian}/Applications/Obsidian.app"
+        "/System/Applications/Calendar.app"
+        "/Applications/Pianoteq 8/Pianoteq 8.app"
+        "/Applications/Dorico 6.app"
+      ];
+      static-only = true;
+    };
     finder.FXPreferredViewStyle = "clmv";
     loginwindow.GuestEnabled = false;
     NSGlobalDomain = {
       AppleICUForce24HourTime = true;
       AppleInterfaceStyle = "Dark";
       KeyRepeat = 2;
+      NSAutomaticCapitalizationEnabled = false;
+      NSAutomaticSpellingCorrectionEnabled = false;
       _HIHideMenuBar = false;
     };
   };
