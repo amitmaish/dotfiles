bda695c428dfb07f038d202df03a9b7473651293
amitmaish <83043520+AmitMaish@users.noreply.github.com>
2026-01-23 10:08:43 -0800

    nix - amit-mbp gen darwin-rebuild --list-generations | grep "current" | awk '{print $1}'

--- nix/hosts/amitmbp/system.nix
+++ nix/hosts/amitmbp/system.nix
@@ -1,29 +1,35 @@
 {pkgs, ...}: {
-  system.defaults = {
-    dock = {
-      autohide = true;
-      persistent-apps = [
-        "/System/Applications/Messages.app"
-        "/Applications/Signal.app"
-        "/Applications/Ghostty.app"
-        "/Applications/Firefox.app"
-        "/Applications/Anki.app"
-        "${pkgs.obsidian}/Applications/Obsidian.app"
-        "/System/Applications/Calendar.app"
-        "/Applications/Pianoteq 8/Pianoteq 8.app"
-        "/Applications/Dorico 6.app"
-      ];
-      static-only = true;
+  system = {
+    defaults = {
+      dock = {
+        autohide = true;
+        persistent-apps = [
+          "/System/Applications/Messages.app"
+          "/Applications/Signal.app"
+          "/Applications/Ghostty.app"
+          "/Applications/Firefox.app"
+          "/Applications/Anki.app"
+          "${pkgs.obsidian}/Applications/Obsidian.app"
+          "/System/Applications/Calendar.app"
+          "/Applications/Pianoteq 8/Pianoteq 8.app"
+          "/Applications/Dorico 6.app"
+        ];
+        static-only = true;
+      };
+      finder.FXPreferredViewStyle = "clmv";
+      loginwindow.GuestEnabled = false;
+      NSGlobalDomain = {
+        AppleICUForce24HourTime = true;
+        AppleInterfaceStyle = "Dark";
+        KeyRepeat = 2;
+        NSAutomaticCapitalizationEnabled = false;
+        NSAutomaticSpellingCorrectionEnabled = false;
+        _HIHideMenuBar = false;
+      };
     };
-    finder.FXPreferredViewStyle = "clmv";
-    loginwindow.GuestEnabled = false;
-    NSGlobalDomain = {
-      AppleICUForce24HourTime = true;
-      AppleInterfaceStyle = "Dark";
-      KeyRepeat = 2;
-      NSAutomaticCapitalizationEnabled = false;
-      NSAutomaticSpellingCorrectionEnabled = false;
-      _HIHideMenuBar = false;
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToEscape = true;
     };
   };
 }
