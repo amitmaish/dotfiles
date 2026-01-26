c873d9ae59e6c30a06c5820c4400024bedcc4a7e
amitmaish <83043520+AmitMaish@users.noreply.github.com>
2026-01-26 15:49:52 -0800

    nix - amit-mbp gen darwin-rebuild --list-generations | grep "current" | awk '{print $1}'

--- nix/hosts/amitmbp/home.nix
+++ nix/hosts/amitmbp/home.nix
@@ -105,6 +105,7 @@ in {
   home.file.".config/nvim".source = mkMutableSymlink ../../../nvim/.config/nvim;
   home.file.".config/starship.toml".source = mkMutableSymlink ../../../starship/.config/starship.toml;
   home.file.".config/yazi".source = mkMutableSymlink ../../../yazi/.config/yazi;
+  home.file.".qutebrowser".source = mkMutableSymlink ../../../qutebrowser;
 
   services.syncthing.enable = true;
 
