4895bffc1e3019722c43d38bc8603cee4d682339
amitmaish <83043520+AmitMaish@users.noreply.github.com>
2026-01-26 16:04:28 -0800

    qutebrowser - init

---
+++ qutebrowser/config.py
@@ -0,0 +1,22 @@
+import os
+from urllib.request import urlopen
+
+# pylint: disable=C0111
+from qutebrowser.config.configfiles import ConfigAPI  # noqa: F401
+from qutebrowser.config.config import ConfigContainer  # noqa: F401
+config: ConfigAPI = config  # noqa: F821 pylint: disable=E0602,C0103
+c: ConfigContainer = c  # noqa: F821 pylint: disable=E0602,C0103
+
+config.load_autoconfig(True)
+
+c.backend = 'webengine'
+
+if not os.path.exists(config.configdir / "theme.py"):
+    theme = "https://raw.githubusercontent.com/catppuccin/qutebrowser/main/setup.py"
+    with urlopen(theme) as themehtml:
+        with open(config.configdir / "theme.py", "a") as file:
+            file.writelines(themehtml.read().decode("utf-8"))
+
+if os.path.exists(config.configdir / "theme.py"):
+    import theme
+    theme.setup(c, 'mocha', True)
