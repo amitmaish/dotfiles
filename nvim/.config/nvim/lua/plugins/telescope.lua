4d0cc24db5a4d53cc4fef85b61a154ca14686959
amitmaish <83043520+AmitMaish@users.noreply.github.com>
2026-01-31 19:31:55 -0800

    nvim - fixed telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,69 +1,23 @@
--- return {
--- 	"nvim-telescope/telescope.nvim",
--- 	dependencies = {
--- 		"nvim-lua/plenary.nvim",
--- 		"JoseConseco/telescope_sessions_picker.nvim",
--- 		"davidgranstrom/telescope-scdoc.nvim",
--- 	},
--- 	opts = {
--- 		extensions = {
--- 			sessions_picker = {
--- 				sessions_dir = vim.fn.stdpath("data") .. "/session/",
--- 			},
--- 		},
--- 	},
--- 	config = function(opts)
--- 		local telescope = require("telescope")
--- 		telescope.setup(opts)
--- 		telescope.load_extension("sessions_picker")
--- 		telescope.load_extension("scdoc")
--- 	end,
--- 	keys = {
--- 		{
--- 			"<leader> ",
--- 			require("telescope.builtin").find_files,
--- 			desc = "telescope files",
--- 		},
--- 		{
--- 			"<leader>pf",
--- 			require("telescope.builtin").git_files,
--- 			desc = "telescope git files",
--- 		},
--- 		{
--- 			"<leader>ph",
--- 			require("telescope.builtin").help_tags,
--- 			desc = "telescope help tags",
--- 		},
--- 		{
--- 			"<leader>pg",
--- 			require("telescope.builtin").live_grep,
--- 			desc = "telescope grep",
--- 		},
--- 		{
--- 			"<leader>pb",
--- 			require("telescope.builtin").builtin,
--- 			desc = "telescope builtins",
--- 		},
--- 		{
--- 			"<leader>ps",
--- 			function()
--- 				vim.cmd.Telescope("sessions_picker")
--- 			end,
--- 			desc = "telescope sessions",
--- 		},
--- 		{
--- 			"<leader>pc",
--- 			require("telescope.builtin").colorscheme,
--- 			desc = "telescope colorschemes",
--- 		},
--- 	},
--- }
-
 return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
+		"JoseConseco/telescope_sessions_picker.nvim",
+		"davidgranstrom/telescope-scdoc.nvim",
 	},
+	opts = {
+		extensions = {
+			sessions_picker = {
+				sessions_dir = vim.fn.stdpath("data") .. "/session/",
+			},
+		},
+	},
+	config = function(opts)
+		local telescope = require("telescope")
+		telescope.setup(opts)
+		telescope.load_extension("sessions_picker")
+		telescope.load_extension("scdoc")
+	end,
 	keys = {
 		{
 			"<leader> ",
@@ -87,6 +41,11 @@ return {
 		},
 		{
 			"<leader>pb",
+			require("telescope.builtin").buffers,
+			desc = "telescope buffers",
+		},
+		{
+			"<leader>pp",
 			require("telescope.builtin").builtin,
 			desc = "telescope builtins",
 		},
