5e1171c0e3942704c10dff2a4e7762c760cd686f
amitmaish <83043520+AmitMaish@users.noreply.github.com>
2026-02-05 12:27:48 -0800

    nix - amit gen

--- nix/apps/dot/dot.nix
+++ nix/apps/dot/dot.nix
@@ -1,8 +1,9 @@
 {
-  writeShellApplication,
+  ncurses5,
   alejandra,
   git,
   libnotify,
+  writeShellApplication,
   REBUILD_COMMAND ? "sudo nixos-rebuild switch --flake ~/dotfiles/nix",
   CURRENT_COMMAND ? ''nu -c "nixos-rebuild list-generations | detect columns | where {|item| \$item.Current == True} | \$in.Generation.0"'',
   NAME ? "nix",
@@ -15,9 +16,8 @@ writeShellApplication {
     alejandra
     git
     libnotify
+    ncurses5
   ];
-
-  runtimeEnv = {inherit NAME REBUILD_COMMAND CURRENT_COMMAND NOTIFY_COMMAND;};
 
   excludeShellChecks = [
     "SC2016"
@@ -25,5 +25,103 @@ writeShellApplication {
     "SC2090"
   ];
 
-  text = builtins.readFile ./dot.sh;
+  text = ''
+    # a script to edit my config and rebuild nix if changes were made
+
+    set -e
+
+    red=$(tput setaf 1)
+    reset_color=$(tput setaf 9)
+
+    force=false
+    git_push=false
+    notify=true
+
+    reset() {
+    	popd >/dev/null
+    	exit "$1"
+    }
+    rebuild() {
+    	${REBUILD_COMMAND} 2>&1 || (
+    		printf "\n%s\n" "''${red}rebuild failed''${reset_color}" && reset 1
+    	)
+    }
+    show_help() {
+    	cat <<-EOF
+    dot: a command to rebuild configs
+    flags:
+    -f  force rebuild even when there weren't changes
+    -h  help
+    -N  disable notificaitons
+    -p  run git pull before editing config
+    -P  git push after config is done
+    -r  rebuild without editing configs
+    	EOF
+    }
+    get_args() {
+    	while getopts "fhNpPr" arg; do
+    		case "$arg" in
+    		f) force=true ;;
+    		h) show_help ;;
+    		N) notify=false ;;
+    		p) git pull ;;
+    		P) git_push=true ;;
+    		r) rebuild && reset 0 ;;
+    		*)
+    			echo "invalid flag: $arg"
+    			show_help
+    			reset 1
+    			;;
+    		esac
+    	done
+    }
+
+    main() {
+    	pushd ~/dotfiles/ >/dev/null
+    	trap '$(reset 0)' EXIT
+
+    	get_args
+
+    	eval "$EDITOR"
+
+    	# exit if unchanged
+    	if [[ ($force == false) && $(git diff --quiet) ]]; then
+    		reset 0
+    	fi
+
+    	# check for nix changes
+    	if [[ ($force == true) || (! $(git diff --quiet "./nix/*")) ]]; then
+
+    		alejandra ./nix/* &>/dev/null || (
+    			printf "\n%s" "''${red}formatting failed!" && reset 1
+    		)
+
+    		git diff -U0 nix/*
+
+    		printf "\n%s\n" "rebuilding config"
+
+    		rebuild
+
+    		current=$(${CURRENT_COMMAND})
+
+    		git commit -am "nix - ${NAME} gen $current" || (
+    			printf "\n%s" "commit failed"
+    			reset 1
+    		)
+
+    		if [[ $git_push == true ]]; then
+    			git push
+    		fi
+
+    		if $notify; then
+    			${NOTIFY_COMMAND}
+    		fi
+
+    	fi
+
+    	reset 0
+    }
+
+    main
+  '';
 }
