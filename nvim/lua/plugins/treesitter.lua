8437dec59b835a3b0cb878d62b074641812a7fa5
amit <amit.maish1@gmail.com>
2026-04-06 12:17:11 -0700

    nvim - updated treesitter for nvim 12

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,26 +1,24 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
-	dependencies = { "tadmccorkle/markdown.nvim" },
+	branch = "main",
 	lazy = false,
 	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			modules = {},
-			ensure_installed = {},
-			auto_install = true,
-			sync_install = false,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = { "markdown" },
-			},
-			ignore_install = {},
-			incremental_selection = {
-				enable = false,
-			},
-			markdown = {
-				enable = true,
-			},
-		})
-	end,
+	opts = {
+		modules = {},
+		ensure_installed = {},
+		auto_install = true,
+		sync_install = false,
+		install_dir = vim.fn.stdpath("data") .. "/site",
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = { "markdown" },
+		},
+		ignore_install = {},
+		incremental_selection = {
+			enable = false,
+		},
+		markdown = {
+			enable = true,
+		},
+	},
 }
