3cc07aa25ac43461b2238c25ba94b9bf5b03ba2a
amit <amit.maish1@gmail.com>
2026-04-07 00:38:33 -0700

    nvim - autostart treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -123,3 +123,16 @@ vim.filetype.add({
 		ncl = "nickel",
 	},
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+
+	pattern = "*",
+
+	callback = function()
+		local filetype = vim.bo.filetype
+
+		if filetype and filetype ~= "" then
+			pcall(vim.treesitter.start)
+		end
+	end,
+})
