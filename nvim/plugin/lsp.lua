8c89f798c3c83e77fe2bacce924efb8d8f9844d4
amit <amit.maish1@gmail.com>
2026-04-07 10:48:46 -0700

    nvim - lsp commands

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -95,3 +95,11 @@ Snacks.toggle
 		end,
 	})
 	:map("<leader>ui")
+
+vim.api.nvim_create_user_command("LspInfo", function()
+	vim.cmd.checkhealth("vim.lsp")
+end, {})
+
+vim.api.nvim_create_user_command("LspLog", function()
+	vim.cmd.tabe("~/.local/state/nvim/lsp.log")
+end, {})
