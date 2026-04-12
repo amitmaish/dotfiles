d69fea40c3f4f0657bdb324249cdb9e66970d5f3
amit <amit.maish1@gmail.com>
2026-04-12 12:46:43 -0700

    nvim - mini.ai treesitter objects

--- nvim/plugin/mini.lua
+++ nvim/plugin/mini.lua
@@ -1,6 +1,15 @@
 vim.pack.add({ "https://github.com/nvim-mini/mini.nvim" })
 
+local spec_treesitter = require("mini.ai").gen_spec.treesitter
 require("mini.ai").setup({
+	custom_textobjects = {
+		F = spec_treesitter({ a = "@function.outer", i = "@function.inner" }),
+		o = spec_treesitter({
+			a = { "@conditional.outer", "@loop.outer" },
+			i = { "@conditional.inner", "@loop.inner" },
+		}),
+	},
+
 	n_lines = 100,
 })
 require("mini.align").setup()
