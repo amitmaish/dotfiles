01d22b84bce5974d426cd5a93c4713b5d80dfb85
amit <amit.maish1@gmail.com>
2026-04-12 20:57:03 -0700

    nvim - changed mini.pick config

--- nvim/plugin/mini.lua
+++ nvim/plugin/mini.lua
@@ -22,7 +22,21 @@ require("mini.pairs").setup({
 		["'"] = false,
 	},
 })
-require("mini.pick").setup()
+local win_config = function()
+	local height = math.floor(0.8 * vim.o.lines)
+	local width = math.floor(0.8 * vim.o.columns)
+	return {
+		anchor = "NW",
+		height = height,
+		width = width,
+		row = math.floor(0.25 * (vim.o.lines - height)),
+		col = math.floor(0.5 * (vim.o.columns - width)),
+	}
+end
+require("mini.pick").setup({
+	options = { content_from_bottom = true },
+	window = { config = win_config },
+})
 require("mini.sessions").setup()
 require("mini.statusline").setup({
 	content = {
