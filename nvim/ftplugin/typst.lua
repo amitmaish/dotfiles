b40890c7338f57c1f78e9f2191a4ef8101db6f00
amit <amit.maish1@gmail.com>
2026-04-19 21:37:00 -0700

    nvim - updates

--- nvim/ftplugin/typst.lua
+++ nvim/ftplugin/typst.lua
@@ -2,3 +2,5 @@ WK.add({
 	{ "<leader>t", group = "typst" },
 	{ "<leader>tp", "<cmd>TypstPreviewToggle<cr>", desc = "toggle typst preview" },
 })
+
+require("mini.pairs").map_buf(0, "i", "_", { action = "closeopen", pair = "__", neigh_pattern = "`^[^\\]`" })
