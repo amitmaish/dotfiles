60335d752f2eafb1e6275cede20f60ebfd4171b6
amit <amit.maish1@gmail.com>
2026-05-19 18:22:11 -0700

    nvim - ensured that there were supercollider snippets before loading them

--- nvim/ftplugin/supercollider.lua
+++ nvim/ftplugin/supercollider.lua
@@ -1,4 +1,6 @@
-require("luasnip").add_snippets("supercollider", require("scnvim/utils").get_snippets())
+if require("scnvim/utils").get_snippets() then
+	require("luasnip").add_snippets("supercollider", require("scnvim/utils").get_snippets())
+end
 
 WK.add({
 	{ "<leader>pS", "<cmd>Telescope scdoc<cr>", desc = "pick supercollider docs" },
